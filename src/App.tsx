691b6392c41f9aa57f3b287b5ae78c6674a788b6
çağatay kılınç <107580070+klncgty@users.noreply.github.com>
2025-01-22 18:13:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ function App() {
         <div className="bg-white text-gray-800 rounded-lg shadow-xl p-8 mb-8">
           <div className="mb-6">
             <img
-              src="3.png"
+              src="/3.png"
               alt="Header Image"
               className="w-full h-auto rounded-lg shadow-md mb-4"
             />
@@ -299,7 +299,7 @@ function App() {
             className="transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
           >
             <img
-              src="4.png"
+              src="/4.png"
               alt="LinkedIn Logo"
               className="w-12 h-12 rounded-full shadow-lg"
             />
